a3e9b08211327529af513c66a49cb04cf1c1f828
Daniel Lewis <github@mrdaniellewis.co.uk>
2020-10-10 15:35:06 +0100

    highlighter update (#12)

--- src/components/highlight_value.jsx
+++ src/components/highlight_value.jsx
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 import { Highlight } from './highlight';
 import { Context } from '../context';
 
-export function HighlightValue({ children: value, highlight, inverse, ...props }) {
-  const context = useContext(Context);
-  const { search, props: { value: _value } } = context;
+export function HighlightValue({ children: value, highlight, inverse, search: _search, ...props }) {
+  const { search, props: { value: _value } } = useContext(Context);
   return (
     <Highlight inverse={inverse}>
-      {highlight(value, search || _value?.label, context, props)}
+      {highlight(value ?? '', _search ?? (search || _value?.label), props)}
     </Highlight>
   );
 }
@@ -17,9 +16,11 @@ HighlightValue.propTypes = {
   children: PropTypes.string,
   highlight: PropTypes.func.isRequired,
   inverse: PropTypes.bool,
+  search: PropTypes.string,
 };
 
 HighlightValue.defaultProps = {
   children: '',
   inverse: false,
+  search: undefined,
 };
