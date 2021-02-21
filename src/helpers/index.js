a9c957bc1b354aa521f1aec43b6bc129ab041359
Daniel Lewis <github@mrdaniellewis.co.uk>
2021-02-21 18:33:39 +0000

    fix inverse highlighting spaces

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export { tokenise } from './tokenise';
+export { inverseHighlight } from './inverse_highlight';
 export { normaliseHighlight } from './normalise_highlight';
 export { toNormalizedString } from './to_normalized_string';
