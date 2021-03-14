74eb367526189dddc5dbbd3a37a20080a7079da7
Daniel Lewis <github@mrdaniellewis.co.uk>
2021-03-14 20:24:01 +0000

    add native datalist example

---
+++ examples/datalist/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const options = [
+  'Apple',
+  'Banana',
+  'Cherry',
+  'Mango',
+  'Orange',
+  'Ugli fruit',
+];
+
+export function Example() {
+  const [value, setValue] = useState(null);
+  return (
+    <>
+      <label htmlFor="input">
+        Choose a fruit
+      </label>
+      <input
+        id="input"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        list="fruits"
+      />
+      <datalist id="fruits">
+        {options.map((option) => (
+          <option key={option} value={option} />
+        ))}
+      </datalist>
+
+      <label htmlFor="output">
+        Current value
+      </label>
+      <output htmlFor="select" id="output">
+        {JSON.stringify(value, undefined, ' ')}
+      </output>
+    </>
+  );
+}
