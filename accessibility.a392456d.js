1eda00997f5a6807c03c9c82c9151eac4c6f49ac
Daniel Lewis <github@mrdaniellewis.co.uk>
2022-08-01 11:09:59 +0100

    Updates

--- accessibility.a392456d.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e=document.querySelector("nav a:not([href])");if(e)for(;;){if(e=e.parentNode.closest("details"),!e)break;e.open=!0}})();
-//# sourceMappingURL=accessibility.a392456d.js.map
