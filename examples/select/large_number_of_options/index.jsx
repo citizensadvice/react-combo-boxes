45db6e5139e8a7d7e691941bd3101cc219e3d0ef
Daniel Lewis <github@mrdaniellewis.co.uk>
2022-12-29 23:13:37 +0000

    add a select with a very large number of options

---
+++ examples/select/large_number_of_options/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { Select } from '../../../src';
+import countries from '../../data/countries.json';
+
+function mapOption({ name, region }) {
+  return { label: name, group: region };
+}
+
+export function Example() {
+  const [value, setValue] = useState(null);
+  return (
+    <>
+      <label htmlFor="select">
+        Select
+      </label>
+      <Select
+        id="select"
+        value={value}
+        onValue={setValue}
+        options={countries}
+        mapOption={mapOption}
+      />
+
+      <label htmlFor="output">
+        Current value
+      </label>
+      <output htmlFor="select" id="output">
+        {JSON.stringify(value, undefined, ' ')}
+      </output>
+    </>
+  );
+}
