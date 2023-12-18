3c0ed6d67a19dbc3e3294f71e9f878e5a550e8aa
Daniel Lewis <github@mrdaniellewis.co.uk>
2023-12-18 18:39:30 +0000

    actually fix the dependency

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,4 @@
 export { layoutColumnsAlignLeft } from './layout_columns_align_left';
 export { layoutMaxWidth } from './layout_max_width';
 export { layoutMaxHeight } from './layout_max_height';
-export { layoutPopover } from './layout_popover';
 export { scrollIntoView } from './scroll_into_view';
