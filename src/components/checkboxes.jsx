c9c80a7a2e89cbd251e1b9ff6be2dc76a9b04fd5
Daniel Lewis <github@mrdaniellewis.co.uk>
2024-06-19 17:13:50 +0100

    fix handling of default props (#122)

--- src/components/checkboxes.jsx
+++ src/components/checkboxes.jsx
@@ -6,206 +6,222 @@ import { classPrefix as defaultClassPrefix } from '../constants/class_prefix';
 import { makeBEMClass } from '../helpers/make_bem_class';
 import { visuallyHiddenClassName } from '../constants/visually_hidden_class_name';
 
-const defaultProps = {
-  classPrefix: `${defaultClassPrefix}checkbox`,
-  groupClassPrefix: `${defaultClassPrefix}checkbox-group`,
-  onValues: undefined,
-  onChange: undefined,
-  values: null,
-  renderWrapper: ({ key, ...props }) => (
-    <div
-      key={key}
-      {...props}
-    />
-  ),
-  renderInput: (props) => <input {...props} />,
-  renderLabel: (props) => <label {...props} />,
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  renderLabelWrapper: (props) => <Fragment {...props} />,
-  renderDescription: (props) => <div {...props} />,
-  renderGroup: ({ key, ...props }) => (
-    <div
-      key={key}
-      {...props}
-    />
-  ),
-  renderGroupLabel: (props) => <div {...props} />,
-  renderGroupAccessibleLabel: (props) => <span {...props} />,
-};
+const defaultRenderWrapper = ({ key, ...props }) => (
+  <div
+    key={key}
+    {...props}
+  />
+);
+const defaultRenderInput = (props) => <input {...props} />;
+const defaultRenderLabel = (props) => <label {...props} />;
+// eslint-disable-next-line react/jsx-no-useless-fragment
+const defaultRenderLabelWrapper = (props) => <Fragment {...props} />;
+const defaultRenderDescription = (props) => <div {...props} />;
+const defaultRenderGroup = ({ key, ...props }) => (
+  <div
+    key={key}
+    {...props}
+  />
+);
+const defaultRenderGroupLabel = (props) => <div {...props} />;
+const defaultRenderGroupAccessibleLabel = (props) => <span {...props} />;
 
-export const Checkboxes = memo((rawProps) => {
-  const optionisedProps = Object.freeze({
-    ...defaultProps,
-    ...rawProps,
-    ...useNormalisedOptions({ ...rawProps, placeholderOption: undefined }),
-  });
-
-  const {
-    classPrefix,
-    groupClassPrefix,
+export const Checkboxes = memo(
+  ({
+    classPrefix = `${defaultClassPrefix}checkbox`,
+    groupClassPrefix = `${defaultClassPrefix}checkbox-group`,
     name,
-    onChange,
     onValues,
-    options,
-    renderWrapper,
-    renderInput,
-    renderLabel,
-    renderLabelWrapper,
-    renderDescription,
-    renderGroup,
-    renderGroupLabel,
-    renderGroupAccessibleLabel,
-    selectedOptions,
-  } = optionisedProps;
-
-  const checkboxRef = useRef(new Map());
-
-  const handleRef = useCallback((identity, el, ref) => {
-    if (el) {
-      checkboxRef.current.set(identity, el);
-    } else {
-      checkboxRef.current.delete(identity);
-    }
-
-    // Allow combination with a passed ref
-    if (typeof ref === 'function') {
-      ref(el);
-    } else if (ref) {
-      ref.current = el;
-    }
-  }, []);
-
-  function handleChange(e) {
-    // When getting the checked values,
-    // we need to get all the current checked values directly from the DOM
-    // If we only use the event target and combine with this selectedOptions
-    // we risk loosing values if selectedOptions is slow to update
-
-    const identities = [];
-    checkboxRef.current.forEach((value, key) => {
-      if (value.checked) {
-        identities.push(key);
-      }
+    onChange,
+    values = null,
+    renderWrapper = defaultRenderWrapper,
+    renderInput = defaultRenderInput,
+    renderLabel = defaultRenderLabel,
+    renderLabelWrapper = defaultRenderLabelWrapper,
+    renderDescription = defaultRenderDescription,
+    renderGroup = defaultRenderGroup,
+    renderGroupLabel = defaultRenderGroupLabel,
+    renderGroupAccessibleLabel = defaultRenderGroupAccessibleLabel,
+    ...props
+  }) => {
+    const normalisedOptions = useNormalisedOptions({
+      values,
+      ...props,
+      placeholderOption: undefined,
     });
-    onValues?.(
-      options
-        .filter((o) => identities.includes(o.identity))
-        .map((o) => o.value),
-    );
-    onChange?.(e);
-  }
 
-  return renderGroupedOptions({
-    options,
-    renderGroup(group) {
-      const { children, key, label, html } = group;
-      return renderGroup(
-        {
-          key,
-          className: makeBEMClass(groupClassPrefix),
-          children: (
-            <>
-              {renderGroupLabel(
-                {
-                  children: label,
-                  className: makeBEMClass(groupClassPrefix, 'label'),
-                  ...html,
-                },
-                { group },
-                optionisedProps,
-              )}
-              {children}
-            </>
-          ),
-        },
-        { group },
-        optionisedProps,
-      );
-    },
-    renderOption(option) {
-      const { identity, label, key, html, disabled, description, group } =
-        option;
-      const checked = selectedOptions.some(
-        (item) => item.identity === identity,
-      );
+    const optionisedProps = Object.freeze({
+      classPrefix,
+      groupClassPrefix,
+      name,
+      onValues,
+      onChange,
+      renderWrapper,
+      renderInput,
+      renderLabel,
+      renderLabelWrapper,
+      renderDescription,
+      renderGroup,
+      renderGroupLabel,
+      renderGroupAccessibleLabel,
+      ...props,
+      normalisedOptions,
+    });
 
-      return renderWrapper(
-        {
-          className: makeBEMClass(classPrefix),
-          children: (
-            <>
-              {renderInput(
-                {
-                  'aria-describedby': description ? `${key}_description` : null,
-                  checked,
-                  disabled,
-                  id: key,
-                  name,
-                  onChange: handleChange,
-                  type: 'checkbox',
-                  value: identity,
-                  className: makeBEMClass(classPrefix, 'input'),
-                  ...html,
-                  ref: (el) => handleRef(identity, el, html?.ref),
-                },
-                { option, checked },
-                optionisedProps,
-              )}
-              {renderLabelWrapper({
-                children: (
-                  <>
-                    {renderLabel(
-                      {
-                        htmlFor: key,
-                        children: (
-                          <>
-                            {group
-                              ? renderGroupAccessibleLabel(
-                                  {
-                                    className: visuallyHiddenClassName,
-                                    children: `${group.label} `,
-                                  },
-                                  { group },
-                                  optionisedProps,
-                                )
-                              : null}
-                            {label}
-                          </>
-                        ),
-                        className: makeBEMClass(classPrefix, 'label'),
-                      },
-                      { option, checked },
-                      optionisedProps,
-                    )}
-                    {description && ' '}
-                    {!!description &&
-                      renderDescription(
+    const { options, selectedOptions } = normalisedOptions;
+
+    const checkboxRef = useRef(new Map());
+
+    const handleRef = useCallback((identity, el, ref) => {
+      if (el) {
+        checkboxRef.current.set(identity, el);
+      } else {
+        checkboxRef.current.delete(identity);
+      }
+
+      // Allow combination with a passed ref
+      if (typeof ref === 'function') {
+        ref(el);
+      } else if (ref) {
+        ref.current = el;
+      }
+    }, []);
+
+    function handleChange(e) {
+      // When getting the checked values,
+      // we need to get all the current checked values directly from the DOM
+      // If we only use the event target and combine with this selectedOptions
+      // we risk loosing values if selectedOptions is slow to update
+
+      const identities = [];
+      checkboxRef.current.forEach((value, key) => {
+        if (value.checked) {
+          identities.push(key);
+        }
+      });
+      onValues?.(
+        options
+          .filter((o) => identities.includes(o.identity))
+          .map((o) => o.value),
+      );
+      onChange?.(e);
+    }
+
+    return renderGroupedOptions({
+      options,
+      renderGroup(group) {
+        const { children, key, label, html } = group;
+        return renderGroup(
+          {
+            key,
+            className: makeBEMClass(groupClassPrefix),
+            children: (
+              <>
+                {renderGroupLabel(
+                  {
+                    children: label,
+                    className: makeBEMClass(groupClassPrefix, 'label'),
+                    ...html,
+                  },
+                  { group },
+                  optionisedProps,
+                )}
+                {children}
+              </>
+            ),
+          },
+          { group },
+          optionisedProps,
+        );
+      },
+      renderOption(option) {
+        const { identity, label, key, html, disabled, description, group } =
+          option;
+        const checked = selectedOptions.some(
+          (item) => item.identity === identity,
+        );
+
+        return renderWrapper(
+          {
+            className: makeBEMClass(classPrefix),
+            children: (
+              <>
+                {renderInput(
+                  {
+                    'aria-describedby': description
+                      ? `${key}_description`
+                      : null,
+                    checked,
+                    disabled,
+                    id: key,
+                    name,
+                    onChange: handleChange,
+                    type: 'checkbox',
+                    value: identity,
+                    className: makeBEMClass(classPrefix, 'input'),
+                    ...html,
+                    ref: (el) => handleRef(identity, el, html?.ref),
+                  },
+                  { option, checked },
+                  optionisedProps,
+                )}
+                {renderLabelWrapper({
+                  children: (
+                    <>
+                      {renderLabel(
                         {
-                          id: `${key}_description`,
-                          children: description,
-                          className: makeBEMClass(classPrefix, 'description'),
+                          htmlFor: key,
+                          children: (
+                            <>
+                              {group
+                                ? renderGroupAccessibleLabel(
+                                    {
+                                      className: visuallyHiddenClassName,
+                                      children: `${group.label} `,
+                                    },
+                                    { group },
+                                    optionisedProps,
+                                  )
+                                : null}
+                              {label}
+                            </>
+                          ),
+                          className: makeBEMClass(classPrefix, 'label'),
                         },
-                        { option, group, checked },
+                        { option, checked },
                         optionisedProps,
                       )}
-                  </>
-                ),
-              })}
-            </>
-          ),
-          key,
-        },
-        { option, group, checked },
-        optionisedProps,
-      );
-    },
-  });
-});
+                      {description && ' '}
+                      {!!description &&
+                        renderDescription(
+                          {
+                            id: `${key}_description`,
+                            children: description,
+                            className: makeBEMClass(classPrefix, 'description'),
+                          },
+                          { option, group, checked },
+                          optionisedProps,
+                        )}
+                    </>
+                  ),
+                })}
+              </>
+            ),
+            key,
+          },
+          { option, group, checked },
+          optionisedProps,
+        );
+      },
+    });
+  },
+);
 
 Checkboxes.propTypes = {
   classPrefix: PropTypes.string,
   groupClassPrefix: PropTypes.string,
   id: PropTypes.string.isRequired,
+  name: PropTypes.string,
   onChange: PropTypes.func,
   onValues: PropTypes.func,
   options: PropTypes.arrayOf(PropTypes.any).isRequired,
--- src/components/select.jsx
+++ src/components/select.jsx
@@ -3,122 +3,136 @@ import PropTypes from 'prop-types';
 import { useNormalisedOptions } from '../hooks/use_normalised_options';
 import { renderGroupedOptions } from '../helpers/render_grouped_options';
 
-const defaultProps = {
-  placeholderOption: null,
-  multiple: false,
-  value: undefined,
-  values: null,
-  onChange: null,
-  onValue: null,
-  onValues: null,
-  renderOption: ({ key, ...props }) => (
-    <option
-      key={key}
-      {...props}
-    />
-  ),
-  renderOptGroup: ({ key, ...props }) => (
-    <optgroup
-      key={key}
-      {...props}
-    />
-  ),
-};
+const defaultRenderOption = ({ key, ...props }) => (
+  <option
+    key={key}
+    {...props}
+  />
+);
+const defaultRenderOptGroup = ({ key, ...props }) => (
+  <optgroup
+    key={key}
+    {...props}
+  />
+);
 
 export const Select = memo(
-  forwardRef((rawProps, ref) => {
-    const optionisedProps = Object.freeze({
-      ...defaultProps,
-      ...rawProps,
-      ...useNormalisedOptions(rawProps, { mustHaveSelection: true }),
-    });
-
-    const {
-      multiple,
-      onChange,
-      onValue,
-      onValues,
-      options,
-      renderOption,
-      renderOptGroup,
-      selectedOption,
-      selectedOptions,
-      value: _0,
-      values: _1,
-      placeholderOption: _2,
-      mapOption: _3,
-      nullOptions: _4,
-      ...selectProps
-    } = optionisedProps;
-
-    const handleChange = useCallback(
-      (e) => {
-        onValues?.(
-          [...e.target.selectedOptions].map(
-            (el) => options.find((o) => (o.identity ?? '') === el.value)?.value,
-          ),
-        );
-        onValue?.(
-          options.find((o) => (o.identity ?? '') === e.target.value)?.value,
-        );
-        onChange?.(e);
+  forwardRef(
+    (
+      {
+        id,
+        options: rawOptions,
+        mapOption,
+        placeholderOption = null,
+        multiple = false,
+        value,
+        values = null,
+        onChange = null,
+        onValue = null,
+        onValues = null,
+        renderOption = defaultRenderOption,
+        renderOptGroup = defaultRenderOptGroup,
+        ...props
       },
-      [onValue, onValues, onChange, options],
-    );
+      ref,
+    ) => {
+      const normalisedOptions = useNormalisedOptions({
+        id,
+        options: rawOptions,
+        placeholderOption,
+        value,
+        values,
+        mapOption,
+        mustHaveSelection: true,
+      });
 
-    const reactValue = useMemo(() => {
-      if (multiple) {
-        return selectedOptions.map((o) => o?.identity ?? '');
-      }
-      return selectedOption?.identity ?? '';
-    }, [selectedOption, selectedOptions, multiple]);
+      const optionisedProps = Object.freeze({
+        id,
+        mapOption,
+        multiple,
+        onChange,
+        onValue,
+        onValues,
+        renderOption,
+        renderOptGroup,
+        ...props,
+        ...normalisedOptions,
+      });
 
-    return (
-      <select
-        value={reactValue}
-        onChange={handleChange}
-        ref={ref}
-        multiple={multiple}
-        {...selectProps}
-      >
-        {renderGroupedOptions({
-          options,
-          renderGroup(group) {
-            const { children, key, label, html } = group;
-            return renderOptGroup(
-              {
-                children,
-                key,
-                label,
-                ...html,
-              },
-              { group },
-              optionisedProps,
-            );
-          },
+      const { options, selectedOption, selectedOptions } = normalisedOptions;
 
-          renderOption(option) {
-            const { identity, label, key, html, disabled, group } = option;
-            return renderOption(
-              {
-                children: label,
-                disabled,
-                key,
-                value: identity,
-                ...html,
-              },
-              { option, group },
-              optionisedProps,
-            );
-          },
-        })}
-      </select>
-    );
-  }),
+      const handleChange = useCallback(
+        (e) => {
+          onValues?.(
+            [...e.target.selectedOptions].map(
+              (el) =>
+                options.find((o) => (o.identity ?? '') === el.value)?.value,
+            ),
+          );
+          onValue?.(
+            options.find((o) => (o.identity ?? '') === e.target.value)?.value,
+          );
+          onChange?.(e);
+        },
+        [onValue, onValues, onChange, options],
+      );
+
+      const reactValue = useMemo(() => {
+        if (multiple) {
+          return selectedOptions.map((o) => o?.identity ?? '');
+        }
+        return selectedOption?.identity ?? '';
+      }, [selectedOption, selectedOptions, multiple]);
+
+      return (
+        <select
+          value={reactValue}
+          onChange={handleChange}
+          ref={ref}
+          multiple={multiple}
+          {...props}
+        >
+          {renderGroupedOptions({
+            options,
+            renderGroup(group) {
+              const { children, key, label, html } = group;
+              return renderOptGroup(
+                {
+                  children,
+                  key,
+                  label,
+                  ...html,
+                },
+                { group },
+                optionisedProps,
+              );
+            },
+
+            renderOption(option) {
+              const { identity, label, key, html, disabled, group } = option;
+              return renderOption(
+                {
+                  children: label,
+                  disabled,
+                  key,
+                  value: identity,
+                  ...html,
+                },
+                { option, group },
+                optionisedProps,
+              );
+            },
+          })}
+        </select>
+      );
+    },
+  ),
 );
 
 Select.propTypes = {
+  id: PropTypes.string,
   placeholderOption: PropTypes.string,
+  mapOption: PropTypes.func,
   multiple: PropTypes.bool,
   onChange: PropTypes.func,
   onValue: PropTypes.func,
