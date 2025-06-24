acd96035178e5efa3827bdd0bea5c3383773f756
Daniel Lewis <daniel.lewis@citizensadvice.org.uk>
2025-06-24 12:28:01 +0100

    Updates

---
+++ examples.67c8d07c.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelector("nav a:not([href])");if(e)for(;e=e.parentNode.closest("details");)e.open=!0})();
+//# sourceMappingURL=examples.67c8d07c.js.map
--- index.c45c639b.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e=document.querySelector("nav a:not([href])");if(e)for(;e=e.parentNode.closest("details");)e.open=!0})();
-//# sourceMappingURL=index.c45c639b.js.map
